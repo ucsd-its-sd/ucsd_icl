97b4ef2d80dbfd1a7765546a8cfb755676dee8a1
Ari Stassinopoulos <stassinopoulosari@gmail.com>
2023-09-17 10:00:27 -0700

    Add argument checking to build script

--- build.sh
+++ build.sh
@@ -1,5 +1,12 @@
-echo "Term: $1";
 ICLTERM=$1
+
+if [ -z "$ICLTERM" ]
+then
+    echo "usage: sh build.sh [TERM]"
+    exit 0
+else
+    echo "Term: $1"
+fi
 
 # https://stackoverflow.com/questions/59895/how-do-i-get-the-directory-where-a-bash-script-is-located-from-within-the-script
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
