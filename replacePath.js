7c5e84f8ac63cb75996baf32a10247d1b78307b1
Ari Stassinopoulos <stassinopoulosari@gmail.com>
2024-02-28 13:44:14 -0500

    Change name on the data classroom

--- replacePath.js
+++ replacePath.js
@@ -1,4 +1,4 @@
-const fs = require('fs').promises,
+const fs = require("fs").promises,
   htmlPath = process.argv[2],
   txtPath = process.argv[3],
   txtHTMLPath = process.argv[4],
@@ -6,28 +6,48 @@ const fs = require('fs').promises,
 
 console.log(htmlPath, txtPath, term);
 
-fs.readFile(txtPath, { encoding: 'utf8' }).then((txtContents) => {
-  const dataStatus = term.padEnd(9, " ") + "Version\n.A00Ari,Stassinopoulos\n0035APM  12345LE1313 08001930A00";
-  fs.writeFile(txtPath, txtContents.split(/\n|\r/g).map(
-    (line, idx) => {
-      if (idx == 1) { return dataStatus + "\n" + line; }
-      return line;
-    }).join('\n')
+fs.readFile(txtPath, { encoding: "utf8" }).then((txtContents) => {
+  const dataStatus =
+    term.padEnd(9, " ") +
+    "Version\n.A00Kevin,Fisher\n0035APM  12345LE1313 08001930A00";
+  fs.writeFile(
+    txtPath,
+    txtContents
+      .split(/\n|\r/g)
+      .map((line, idx) => {
+        if (idx == 1) {
+          return dataStatus + "\n" + line;
+        }
+        return line;
+      })
+      .join("\n"),
   );
 });
 
-fs.readFile(htmlPath, { encoding: 'utf8' }).then((htmlContents) => {
+fs.readFile(htmlPath, { encoding: "utf8" }).then((htmlContents) => {
   var nextLineReplace = false;
-  fs.writeFile(htmlPath, htmlContents.split(/\n|\r/g).map((line) => {
-    if (nextLineReplace) {
-      console.log("replace line: `" + line + "` with `    window.icl.CURRENT_PATH = \"" + txtHTMLPath + "\";`");
-      nextLineReplace = false;
-      return "    window.icl.CURRENT_PATH = \"" + txtHTMLPath + "\";";
-    }
-    if (line.includes("MARK-NEXTLINEPATH")) {
-      console.log("replace next line");
-      nextLineReplace = true;
-    }
-    return line;
-  }).join('\n'));
+  fs.writeFile(
+    htmlPath,
+    htmlContents
+      .split(/\n|\r/g)
+      .map((line) => {
+        if (nextLineReplace) {
+          console.log(
+            "replace line: `" +
+              line +
+              '` with `    window.icl.CURRENT_PATH = "' +
+              txtHTMLPath +
+              '";`',
+          );
+          nextLineReplace = false;
+          return '    window.icl.CURRENT_PATH = "' + txtHTMLPath + '";';
+        }
+        if (line.includes("MARK-NEXTLINEPATH")) {
+          console.log("replace next line");
+          nextLineReplace = true;
+        }
+        return line;
+      })
+      .join("\n"),
+  );
 });
