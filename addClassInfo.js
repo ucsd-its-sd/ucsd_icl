47907d012baf1735749c02fe8c252b604cdf324b
ari stassinopoulos <42446375+stassinopoulosari@users.noreply.github.com>
2024-04-23 09:20:55 -0400

    Simplify code to add data class

--- addClassInfo.js
+++ addClassInfo.js
@@ -2,22 +2,18 @@ const fs = require("fs").promises,
   txtPath = process.argv[2],
   term = process.argv[3];
 
-console.log(htmlPath, txtPath, term);
-
 fs.readFile(txtPath, { encoding: "utf8" }).then((txtContents) => {
   const dataStatus =
     term.padEnd(9, " ") +
-    "Version\n.A00Kevin,Fisher\n0035APM  12345LE1313 08001930A00";
+    "Version\n.A00Kevin,Fisher\n0035APM  12345LE1313 08001930A00",
+    splitText = txtContents.split(/\n|\r/g)
   fs.writeFile(
     txtPath,
-    txtContents
-      .split(/\n|\r/g)
-      .map((line, idx) => {
-        if (idx == 1) {
-          return dataStatus + "\n" + line;
-        }
-        return line;
-      })
+    [].concat(
+      splitText[0],
+      dataStatus,
+      splitText.slice(1)
+    )
       .join("\n"),
   );
 });
