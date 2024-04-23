31fa1f6be5755f762ed816869c270f6a1b6f0a75
ari stassinopoulos <42446375+stassinopoulosari@users.noreply.github.com>
2024-04-23 09:13:34 -0400

    Update and rename replacePath.js to addClassInfo.js
    Remove commented code

---
+++ addClassInfo.js
@@ -0,0 +1,25 @@
+const fs = require("fs").promises,
+  htmlPath = process.argv[2],
+  txtPath = process.argv[3],
+  txtHTMLPath = process.argv[4],
+  term = process.argv[5];
+
+console.log(htmlPath, txtPath, term);
+
+fs.readFile(txtPath, { encoding: "utf8" }).then((txtContents) => {
+  const dataStatus =
+    term.padEnd(9, " ") +
+    "Version\n.A00Kevin,Fisher\n0035APM  12345LE1313 08001930A00";
+  fs.writeFile(
+    txtPath,
+    txtContents
+      .split(/\n|\r/g)
+      .map((line, idx) => {
+        if (idx == 1) {
+          return dataStatus + "\n" + line;
+        }
+        return line;
+      })
+      .join("\n"),
+  );
+});
